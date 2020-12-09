68beacbbcca2268a1e4ce7b5242d1559c3dd22e5
Edward Waterman <edward.waterman@posteo.de>
2020-12-09 16:19:26 +0100

    F# Day 9

---
+++ fsharp/day9.fsx
@@ -0,0 +1,37 @@
+open System.IO
+open FSharp.Collections
+
+let isAncestorSum (ancestor: uint64 []) (target: uint64): bool =
+    let map =
+        Seq.map (fun num -> num, ()) ancestor |> Map.ofSeq
+
+    Map.exists (fun key _ -> Map.containsKey (target - key) map) map
+
+let invalidNumber (data: uint64 []): uint64 option =
+    Seq.windowed (26) data
+    |> Seq.tryPick (fun window ->
+        match not (isAncestorSum window.[0..25] window.[25]) with
+        | true -> Some window.[25]
+        | _ -> None)
+
+let invalidSummands (data: uint64 []) (target: uint64): uint64 [] option =
+    seq { 2 .. data.Length }
+    |> Seq.tryPick (fun i ->
+        Seq.windowed (i) data
+        |> Seq.tryFind (fun window -> Seq.sum window = target))
+
+let data =
+    File.ReadAllLines "../input/day9.txt"
+    |> Array.map (uint64)
+
+let invalid =
+    match invalidNumber data with 
+    | Some number ->
+        ignore (printfn "Solution 1: %d" number)
+        number
+    | _ -> failwith "No invalid number found"
+
+
+match invalidSummands data invalid with
+| Some summands -> printfn "Solution 2: %d" ((Seq.min summands) + (Seq.max summands))
+| None -> failwith "No summands found"
--- rust/day9/src/main.rs
+++ rust/day9/src/main.rs
@@ -11,11 +11,10 @@ fn parse(input: &str) -> Vec<usize> {
 
 fn sum_of_ancestors_exists(ancestors: &[usize], target: usize) -> bool {
     // See https://stackoverflow.com/questions/4720271/find-a-pair-of-elements-from-an-array-whose-sum-equals-a-given-number
-    let map = {
-        let mut map = HashMap::with_capacity(25);
-        map.extend(ancestors.iter().enumerate().map(|(idx, val)| (*val, idx)));
-        map
-    };
+    let map: HashMap<usize, ()> = ancestors
+        .iter()
+        .map(|val| (*val, ()))
+        .collect();
     map.keys().any(|key| map.contains_key(&(target - key)))
 }
 
@@ -44,10 +43,13 @@ fn main() {
     let solution1 = invalid_number(&input).expect("No invalid number found!");
     println!("Solution 1: {}", solution1);
     let solution2 = {
-        let summands = invalid_number_summands(&input, solution1).expect("No continuous slice found");
+        let summands =
+            invalid_number_summands(&input, solution1).expect("No continuous slice found");
         let min = summands.iter().min();
         let max = summands.iter().max();
-        min.zip(max).map(|(min, max)| min + max).expect("Empty list")
+        min.zip(max)
+            .map(|(min, max)| min + max)
+            .expect("Empty list")
     };
     println!("Solution 2: {}", solution2);
 }
@@ -66,10 +68,13 @@ mod tests {
         let input = parse(INPUT);
         let solution1 = invalid_number(&input).expect("No invalid number found!");
         let solution2 = {
-            let summands = invalid_number_summands(&input, solution1).expect("No continuous slice found");
+            let summands =
+                invalid_number_summands(&input, solution1).expect("No continuous slice found");
             let min = summands.iter().min();
             let max = summands.iter().max();
-            min.zip(max).map(|(min, max)| min + max).expect("Empty list")
+            min.zip(max)
+                .map(|(min, max)| min + max)
+                .expect("Empty list")
         };
         assert_eq!(solution2, 13935797)
     }
